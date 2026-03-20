f5001dcabcae8e597dfac600bb9aed220bf11fe8
unknown <mazidatou99@gmail.com>
2026-03-19 20:10:12 -0500

    updated my resume

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ function toggleMenue(){
    menu.classList.toggle("open");
    icon.classList.toggle("open");
 }
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = sec.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      sec.classList.add("show");
+    }
+  });
+});
